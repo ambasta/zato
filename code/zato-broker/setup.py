b88a4b77f00d1bd563b5e01e9013e57845e271c7
Amit Prakash Ambasta <amit.ambasta@delhivery.com>
2014-06-30 17:52:47 +0530

    Updated setup to handle git builds

--- code/zato-broker/setup.py
+++ code/zato-broker/setup.py
@@ -7,22 +7,111 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # flake8: noqa
-import os
-from setuptools import setup, find_packages
+import inspect, json, os, re, subprocess
 
-curdir = os.path.dirname(os.path.abspath(__file__))
-_version_py = os.path.normpath(os.path.join(curdir, '..', '.version.py'))
-_locals = {}
-execfile(_version_py, _locals)
-version = _locals['version']
+from distutils.core import setup, Command
+from distutils.command.sdist import sdist as _sdist
+
+from setuptools import find_packages
+
+VERSION_PY = '''
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distribution tarballs contain a pre-generated copy of this file.
+
+__version__ = '{}.{}.{}.rev-{}'
+'''
+
+PACKAGE = 'zato-server'
+GIT_PATH = '../../.git'
+
+DESC = 'Convenience Python client for Zato ESB and app server (https://zato.io)'
+
+
+def update_version_py():
+    if not os.path.isdir(GIT_PATH):
+        print "This does not appear to be a Git repository."
+        return
+    try:
+        p = subprocess.Popen([
+           'git',
+           'log',
+           '--pretty=format:\'%h\'',
+           '-n 1',],
+            stdout=subprocess.PIPE)
+    except EnvironmentError:
+        print('Unable to run git, leaving _version.py alone')
+        return
+
+    stdout = p.communicate()[0]
+
+    if p.returncode != 0:
+        print('Unable to run git, leaving _version.py alone')
+
+    revision = stdout[1:-1].strip()
+
+    # Evaluate release.json in curdir/../release_info/release.json
+    _file = inspect.currentframe().f_code.co_filename
+    curdir = os.path.dirname(os.path.abspath(_file))
+    release_info_dir = os.path.join(curdir, '../release-info')
+    release = json.loads(open(os.path.join(release_info_dir, 'release.json')).read())
+
+    f = open('_version.py', 'w')
+    f.write(VERSION_PY.format(release['major'], release['minor'], release['micro'], revision))
+    f.close()
+
+    print 'set {}/_version.py to {}.{}.{}.rev-{}'.format(
+        PACKAGE,
+        release['major'],
+        release['minor'],
+        release['micro'],
+        revision)
+
+
+def get_version():
+    try:
+        f = open('_version.py')
+    except EnvironmentError:
+        return None
+
+    for line in f.readlines():
+        mo = re.match("__version__ = '([^']+)'", line)
+        if mo:
+            ver = mo.group(1)
+            return ver
+    return None
+
+
+class Version(Command):
+    description = 'Update _version.py from Git repo'
+    user_options = []
+    boolean_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        update_version_py()
+        print('Version is now {}'.format(get_version()))
+
+
+class sdist(_sdist):
+    def run(self):
+        update_version_py()
+        self.distribution.metadata.version = get_version()
+        return _sdist.run(self)
+
 
 setup(
-      name = 'zato-broker',
-      version = version,
-      author = 'Zato Developers',
-      author_email = 'info@zato.io',
-      url = 'https://zato.io',
-      package_dir = {'':'src'},
-      packages = find_packages('src'),
-      namespace_packages = ['zato'],
+    name = 'zato-broker',
+    version = get_version(),
+    author = 'Zato Developers',
+    author_email = 'info@zato.io',
+    url = 'https://zato.io',
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
+    namespace_packages = ['zato'],
+    cmdclass={'version': Version, 'sdist': sdist },
 )
