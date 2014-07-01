7ad7d387bd9720f8ad210b6c7b319cb700c94f2e
Amit Prakash Ambasta <amit.ambasta@delhivery.com>
2014-07-01 14:19:07 +0530

    Delete unneeded code

--- code/hotfixman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=1.1
-
-mkdir -p ./hotfixes/backups-$VERSION
-rm -rf ./hotfixes/hotfixes-$VERSION
-curl https://zato.io/hotfixes/hotfixes-$VERSION.tar.bz2 -o ./hotfixes/hotfixes-$VERSION.tar.bz2
-tar xvfj ./hotfixes/hotfixes-$VERSION.tar.bz2 -C ./hotfixes
-rsync -abvv --suffix=_`date +"%Y%m%d_%H_%M_%S"` --backup-dir ./hotfixes/backups-$VERSION hotfixes/hotfixes-$VERSION/ .
